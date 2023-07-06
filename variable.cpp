ab2d2fc311c81a9bb8028d9773d5b8e7478a8f15
Ayush Raj <rajayush1412@gmail.com>
2023-07-06 15:32:04 +0530

    basics about variable

---
+++ variable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a=14, b=15;
+    float pi=3.14;
+    char c='a';
+    cout<<"the value of a is "<<a<<"\nthe value of b is "<<b;
+    cout<<"\nthe value of pi is "<<pi<<"\nthe value of c is "<<c;    
+
+    return 0;
+}
