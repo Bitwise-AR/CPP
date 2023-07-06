ff1db7d82002d440545cdd06e1a2bb59d68c1537
Ayush Raj <rajayush1412@gmail.com>
2023-07-06 19:30:16 +0530

    float, double and long double literals

---
+++ literals.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    float d=34.4;
+    long double e=34.4;
+    //by default the value is taken as double if there is no typecasting
+    cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl;
+    cout<<"The size of 34.4f is "<<sizeof(34.4f)<<endl;
+    cout<<"The size of 34.4F is "<<sizeof(34.4F)<<endl;
+    cout<<"The size of 34.4l is "<<sizeof(34.4l)<<endl;
+    cout<<"The size of 34.4L is "<<sizeof(34.4L)<<endl;
+}
