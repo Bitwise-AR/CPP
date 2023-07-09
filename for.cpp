fc379cd4fe5d99deb7213626113e9b57ca71000f
Ayush Raj <rajayush1412@gmail.com>
2023-07-09 20:07:22 +0530

    Basic structure of FOR loop

---
+++ for.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+//Syntax for "FOR" loop
+// for (initialization; condition; updation)
+// {
+//     LOOP body(C++ Code)
+// }
+
+int main(){
+    for (int i = 0; i < 4; i++)
+    {
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
