698e1ddb3d2d0654bfd810bfc27336784b78fc0c
Ayush Raj <rajayush1412@gmail.com>
2023-07-09 20:13:52 +0530

    Basic structure of WHILE loop

---
+++ while.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+//Syntax for "WHILE" loop
+// while(CONDITION)
+// {
+//     C++ statements
+// }
+
+int main(){
+    int i=1;
+    while (i<=40)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    
+    return 0;
+}
